902433d0753cf76a685f5b3404b160f477143fbc
Rahul <rahulpanditrbp24@gmail.com>
2025-12-19 14:56:14 +0530

    GreetingCard_01

--- 01_GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ 01_GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.greetingcard.ui.theme.GreetingCardTheme
 
-class MainActivity : ComponentActivity() {
+    class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -33,10 +35,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Box {
+    Surface(color = Color.Cyan) {
         Text(
-            text = "Hi, my name is $name",
-            modifier = modifier
+            text = "Hi, my name is $name!",
+            modifier = modifier.padding(24.dp)
         )
     }
 }
